1dc037c9e1280e572ba5e133f3ea903740fb1237
Justin Allred <allredjustin@hotmail.com>
2026-04-04 19:06:54 -0700

    Fix import order

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
 from functools import wraps
 from typing import TYPE_CHECKING, Any
 
-from unrealsdk.hooks import Type
-from unrealsdk.unreal import BoundFunction, UObject, WrappedStruct
-
 from mods_base import EInputEvent, KeybindType, hook
 from mods_base.mod_list import base_mod
+from unrealsdk.hooks import Type
+from unrealsdk.unreal import BoundFunction, UObject, WrappedStruct
 
 if TYPE_CHECKING:
     from mods_base.keybinds import KeybindCallback_Event, KeybindCallback_NoArgs
